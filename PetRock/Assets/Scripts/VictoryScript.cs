875ab55f32157db0200cfe1372b95208a8530eca
Elise-Nguyen <elise.nguyen@edu.devinci.fr>
2021-10-06 14:00:43 +0200

    Ajout de VIctory et Defeat trigger
    + Prefab correspondant

---
+++ PetRock/Assets/Scripts/VictoryScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        //Debug.Log("C'est la victoire");
+        SceneManager.LoadScene("Victory_Scene");
+    }
+
+}
