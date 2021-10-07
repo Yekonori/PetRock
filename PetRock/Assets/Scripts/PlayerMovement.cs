a6b75180b835cbb9581659863f7e9a972525da39
Alexis <alexis.sainsard@gmail.com>
2021-10-07 09:08:12 +0200

    dead zone in input

--- PetRock/Assets/Scripts/PlayerMovement.cs
+++ PetRock/Assets/Scripts/PlayerMovement.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     #region Script Parameters
-
-    [Range(0f, 0.5f)] float deadZone = 0.4f;
-
     [Header("..")]
     [SerializeField, Min(0f)] float speed = 5f;
     [SerializeField, Min(0f)] float rotationSpeed = 90f;
@@ -31,7 +28,7 @@ public class PlayerMovement : MonoBehaviour
 
     public void SetMovementDirection(float vertical, float rotate)
     {
-        _moveForward = Mathf.Abs(vertical) > deadZone ? (int)Mathf.Sign(vertical) : 0;
-        _moveRotation = Mathf.Abs(rotate) > deadZone ? (int)Mathf.Sign(rotate) : 0; ;
+        _moveForward = (int)vertical;
+        _moveRotation = (int)rotate;
     }
 }
