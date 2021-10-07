a1bd8e4e850d131361478321d3ee54c517900e19
Elise-Nguyen <elise.nguyen@edu.devinci.fr>
2021-10-07 12:17:23 +0200

    Ajout de l'action Ramasser

---
+++ PetRock/Assets/Scripts/RockScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockScript : MonoBehaviour
+{
+    public Transform player;
+    public float distanceDetection = 2f;
+
+    private bool hasPlayer = false;
+    private bool beingCarried = false;
+
+    private float t = 0;
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Calcule la distance entre Pierre et joueur
+        float dist = Vector3.Distance(gameObject.transform.position, player.position);
+
+
+        //Distance de ramassage
+        if (dist <= distanceDetection)
+        {
+            hasPlayer = true;
+        }
+        else
+        {
+            hasPlayer = false;
+        }
+
+        // si on peut ramasser et qu'on appuie sur A
+        if (hasPlayer && Input.GetKeyDown(KeyCode.A))
+        {
+            if (!beingCarried)
+            {
+                GetComponent<Rigidbody>().isKinematic = true;
+                transform.parent = player;
+                beingCarried = true;
+            }
+            else
+            {
+                Debug.Log("Tu m'as lâché");
+                GetComponent<Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                beingCarried = false;
+            }
+        }
+
+        if (beingCarried)
+        {
+            Debug.Log("Je suis porté");
+            transform.position = player.transform.position;
+
+        }
+
+    }
+}
