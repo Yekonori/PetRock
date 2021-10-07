77c1b764993b99152806f82fc5c1be9a91bcc107
Maxence <mxcolas302@gmail.com>
2021-10-07 11:07:14 +0200

    Add pause menu

---
+++ PetRock/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class GameManager : MonoBehaviour
+{
+    [BoxGroup("Pause Menu Paramaters")]
+    public GameObject _pauseMenu;
+    [HideInInspector]
+    public bool _inPause = false;
+
+    public static GameManager instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PauseGame()
+    {
+        _inPause = true;
+        GameObject pMenu = Instantiate(_pauseMenu, GameObject.FindGameObjectWithTag("Canvas").transform);
+        pMenu.transform.localPosition = new Vector3(1500, 0, 0);
+    }
+}
