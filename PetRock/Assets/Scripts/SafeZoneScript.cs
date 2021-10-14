017d152306755f792b5c046de5d19d2ae743f862
Elise-Nguyen <elise.nguyen@edu.devinci.fr>
2021-10-14 09:39:01 +0200

    Script safe zone et ajout bool pour joueur

---
+++ PetRock/Assets/Scripts/SafeZoneScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeZoneScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("Player enter");
+            other.GetComponent<PlayerMovement>().SetSafe(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Player out");
+            other.GetComponent<PlayerMovement>().SetSafe(false); ;
+        }
+    }
+}
