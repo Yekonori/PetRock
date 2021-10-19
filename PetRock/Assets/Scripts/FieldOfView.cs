a08484982d2f777c0dd3b14861865b7a6e2bbffc
Alexis <alexis.sainsard@gmail.com>
2021-10-19 11:03:20 +0200

    better handle edge

--- PetRock/Assets/Scripts/FieldOfView.cs
+++ PetRock/Assets/Scripts/FieldOfView.cs
@@ -13,6 +13,8 @@ public class FieldOfView : MonoBehaviour
     public LayerMask obstableLayerMask;
 
     public float meshResolution = 0.25f;
+    public int edgeResolveIteration = 4;
+    public float edgeDistanceThreshold = 0.5f;
     public MeshFilter viewMeshFilter;
     Mesh viewMesh;
 
@@ -90,14 +92,35 @@ public class FieldOfView : MonoBehaviour
     {
         int stepCount = Mathf.RoundToInt(meshResolution * viewAngle);
         float stepAngleSize = viewAngle / stepCount;
-        List<Vector3> viewPoints = new List<Vector3>(); 
+        List<Vector3> viewPoints = new List<Vector3>();
+        ViewCastInfo oldViewCast = new ViewCastInfo();
 
         for (int i = 0; i<stepCount; ++i)
         {
             float angle = transform.eulerAngles.y - viewAngle / 2 + i * stepAngleSize;
             ViewCastInfo newViewCast = ViewCast(angle);
+
+            if (i > 0)
+            {
+                bool edgeDistanceThresholdExceeded = Mathf.Abs(oldViewCast.distance - newViewCast.distance) > edgeDistanceThreshold;
+
+                if (oldViewCast.hit != newViewCast.hit || (oldViewCast.hit && newViewCast.hit && edgeDistanceThresholdExceeded))
+                {
+                    EdgeInfo edge = FindEdge(oldViewCast, newViewCast);
+
+                    if (edge.pointA != Vector3.zero)
+                    {
+                        viewPoints.Add(edge.pointA);
+                    }
+                    if (edge.pointB != Vector3.zero)
+                    {
+                        viewPoints.Add(edge.pointB);
+                    }
+                }
+            }
+
             viewPoints.Add(newViewCast.point);
-            //Debug.DrawLine(transform.position, transform.position + DirFromAngle(angle, true) * range, Color.red);
+            oldViewCast = newViewCast;
         }
 
         int vertexCount = stepCount + 1;
@@ -153,5 +176,46 @@ public class FieldOfView : MonoBehaviour
     {
         range = _range;
         viewAngle = _angle;
+    }
+
+    public struct EdgeInfo
+    {
+        public Vector3 pointA;
+        public Vector3 pointB;
+
+        public EdgeInfo(Vector3 A, Vector3 B)
+        {
+            pointA = A;
+            pointB = B;
+        }
+    }
+
+    EdgeInfo FindEdge(ViewCastInfo minViewCast, ViewCastInfo maxViewCast)
+    {
+        float minAngle = minViewCast.angle;
+        float maxAngle = maxViewCast.angle;
+        Vector3 minPoint = Vector3.zero;
+        Vector3 maxPoint = Vector3.zero;
+
+        for (int i = 0; i < edgeResolveIteration; ++i)
+        {
+            float angle = (minAngle + maxAngle) / 2;
+            ViewCastInfo newViewCast = ViewCast(angle);
+
+            bool edgeDistanceThresholdExceeded = Mathf.Abs(minViewCast.distance - newViewCast.distance) > edgeDistanceThreshold;
+
+            if (newViewCast.hit == minViewCast.hit && !edgeDistanceThresholdExceeded)
+            {
+                minAngle = angle;
+                minPoint = newViewCast.point;
+            }
+            else
+            {
+                maxAngle = angle;
+                maxPoint = newViewCast.point;
+            }
+        }
+
+        return new EdgeInfo(minPoint, maxPoint);
     }
 }
