e7dacab86564a6e9dc5023c7e6903df348315ffd
Maxence <mxcolas302@gmail.com>
2021-10-21 21:24:44 +0200

    Fix player trigger | add RBZone

---
+++ PetRock/Assets/Scripts/Player_Scripts/PlayerTriggers.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTriggers : MonoBehaviour
+{
+    private const string _safeZoneTag = "SafeZone";
+    private const string _rockBalancingZoneTag = "RockBalancing";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case _safeZoneTag:
+                other.gameObject.GetComponent<SafeZoneScript>().EnterOnSafeZone();
+                break;
+
+            case _rockBalancingZoneTag:
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == _safeZoneTag)
+            other.gameObject.GetComponent<SafeZoneScript>().ExitSafeZone();
+    }
+}
