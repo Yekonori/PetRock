7d1ec2fe0b5b863668af0353c8180c1fdbf75d82
Alexis <alexis.sainsard@gmail.com>
2021-11-08 08:09:11 +0100

    create a dolllyView ta move on the rail

---
+++ PetRock/Assets/Scripts/Camera/Views/DollyView/DollyViewAutoCircle.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DollyViewAutoCircle : AView
+{
+    public float roll = 0f;
+    public float distance = 0f;
+    public float fov = 0f;
+
+    [SerializeField] Transform target;
+    [SerializeField] Rail rail;
+    [SerializeField] float timeToRotate = 5;
+
+    private Vector3 railPosition = Vector3.zero;
+
+    private float yaw = 0f;
+    private float pitch = 0f;
+
+    private float railLength = 0f;
+    private float timer = 0f;
+
+    void Start()
+    {
+        railLength = rail.GetLength();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActive)
+        {
+            timer += Time.deltaTime;
+
+            railPosition = rail.GetPosition(timer / timeToRotate * railLength);
+
+            Quaternion rot = Quaternion.LookRotation(target.position - railPosition);
+            yaw = rot.eulerAngles.y;
+            pitch = rot.eulerAngles.x;
+        }
+    }
+
+    public override CameraConfiguration GetConfiguration()
+    {
+        return new CameraConfiguration(yaw, pitch, roll, railPosition, distance, fov);
+    }
+}
