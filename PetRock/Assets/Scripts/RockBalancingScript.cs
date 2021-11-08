54135ff02c640519576370bb910af1801c43c31d
Maxence <mxcolas302@gmail.com>
2021-11-08 09:59:48 +0100

    rb fix

--- PetRock/Assets/Scripts/RockBalancingScript.cs
+++ PetRock/Assets/Scripts/RockBalancingScript.cs
@@ -7,7 +7,7 @@ public class RockBalancingScript : MonoBehaviour
 {
     [HideInInspector]
     public Player player;
-    [Range(0f, 0.5f)] float deadZone = 0.01f;
+    [Range(0f, 0.5f)] float _deadZone = 0.01f;
 
     // direction and rotation
     private int _moveForward = 0;
@@ -15,51 +15,65 @@ public class RockBalancingScript : MonoBehaviour
 
     //Speed rock
     [Header("Speed rock movement")]
-    [SerializeField, Min(0f)] float speed = 5f;
-    [SerializeField, Min(0f)] float rotationSpeed = 90f;
+    [SerializeField, Min(0f)] float _speed = 5f;
+    [SerializeField, Min(0f)] float _rotationSpeed = 90f;
 
     [Header("Start rock position")]
     [SerializeField]
-    private Transform startPosRock;
+    private Transform _startPosRock;
 
     [Header("Validate position rock")]
     [SerializeField]
-    private Transform finalPosRock;
+    private Transform _finalPosRock;
+    private Transform _finalA;
+    private Transform _finalB;
 
     [Header("RockPrefab")]
     [SerializeField]
-    private GameObject prefabRock;
-    private GameObject theRock;
+    private GameObject _prefabRock;
+    private GameObject _theRock;
 
-    private bool canRockBalance = false;
+    private bool _canRockBalance = false;
+    private bool _goodPlace = false;
+    private bool _goodRot = false;
+
+    [Header("Camera views")]
+    [SerializeField]
+    private TriggeredViewVolume _triggeredViewVolume;
+    [SerializeField]
+    private DollyViewAutoCircle _dollyView;
 
     private void OnEnable()
     {
         player = GameManager.instance.player;
-        theRock = Instantiate(prefabRock, startPosRock.position, Quaternion.identity, startPosRock.parent);
+        _theRock = Instantiate(_prefabRock, _startPosRock.position, Quaternion.identity, _startPosRock.parent);
+
+        _finalA = _finalPosRock.GetChild(0);
+        _finalB = _finalPosRock.GetChild(1);
+
         StartCoroutine(startRockBalance());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!canRockBalance)
+        if (!_canRockBalance)
             return;
 
         float move = player.GetAxis("MovePetRock");
-        int moveInt = Mathf.Abs(move) > deadZone ? (int)Mathf.Sign(move) : 0;
+        int moveInt = Mathf.Abs(move) > _deadZone ? (int)Mathf.Sign(move) : 0;
 
         float rotate = player.GetAxis("TiltPetRock");
-        int rotateInt = Mathf.Abs(rotate) > deadZone ? (int)Mathf.Sign(rotate) : 0;
+        int rotateInt = Mathf.Abs(rotate) > _deadZone ? (int)Mathf.Sign(rotate) : 0;
 
         SetMovementDirection(moveInt, rotateInt);
 
-        theRock.transform.position += _moveForward * transform.forward * speed * Time.deltaTime; // += gravity * 
-        theRock.transform.Rotate(Vector3.up, _moveRotation * rotationSpeed * Time.deltaTime);
+        _theRock.transform.position += _moveForward * transform.forward * _speed * Time.deltaTime; // += gravity * 
+        _theRock.transform.Rotate(Vector3.up, _moveRotation * _rotationSpeed * Time.deltaTime);
 
         if (ValidatePos() && player.GetButton("ValidatePetRockPos"))
         {
-            Debug.LogError("here");
+            EndRockBalancing();
         }
     }
 
@@ -71,13 +85,34 @@ public class RockBalancingScript : MonoBehaviour
 
     private bool ValidatePos()
     {
-        return theRock.transform.position == finalPosRock.position && 
-            theRock.transform.rotation == finalPosRock.rotation;
+        if (_theRock.transform.position.z <= _finalA.position.z && _theRock.transform.position.z >= _finalB.position.z)
+            _goodPlace = true;
+
+        if (_theRock.transform.rotation == _finalPosRock.rotation)
+            _goodRot = true;
+
+        return _goodPlace && _goodRot;
     }
 
     private IEnumerator startRockBalance()
     {
         yield return new WaitForSeconds(1);
-        canRockBalance = true;
+        _canRockBalance = true;
+    }
+
+    private void EndRockBalancing()
+    {
+        _triggeredViewVolume.view = _dollyView;
+        _triggeredViewVolume.ActiveView(true);
+        StartCoroutine(finishRockBalancing());
+    }
+
+    private IEnumerator finishRockBalancing()
+    {
+        yield return new WaitForSeconds(_dollyView.getTimeToRotate + 1);
+        GetComponent<Collider>().enabled = false;
+        _triggeredViewVolume.ActiveView(false);
+        enabled = false;
+        GameManager.instance.inRockBalancing = false;
     }
 }
