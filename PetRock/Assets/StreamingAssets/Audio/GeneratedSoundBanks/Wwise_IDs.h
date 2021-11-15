39498c0361fb692f1372ef0771956ca38eefe7df
Louise Jeanson <77324204+Noom0@users.noreply.github.com>
2021-11-15 13:57:03 +0100

    [SOUND] Set up wwise

--- PetRock/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ PetRock/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,7 +14,38 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID PLAY_DUCK = 2611198821U;
+        static const AkUniqueID PLAY_MC_FT = 206389491U;
     } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace MC_FT_SURFACE
+        {
+            static const AkUniqueID GROUP = 3596758950U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID MARBLE = 1127618254U;
+                static const AkUniqueID SAND = 803837735U;
+                static const AkUniqueID STONE = 1216965916U;
+                static const AkUniqueID WATER = 2654748154U;
+            } // namespace SWITCH
+        } // namespace MC_FT_SURFACE
+
+        namespace MC_TYPE
+        {
+            static const AkUniqueID GROUP = 1750504232U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID RUN = 712161704U;
+                static const AkUniqueID STEALTH = 2909291642U;
+                static const AkUniqueID WALK = 2108779966U;
+            } // namespace SWITCH
+        } // namespace MC_TYPE
+
+    } // namespace SWITCHES
 
     namespace BANKS
     {
@@ -24,7 +55,14 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID AMB = 1117531639U;
+        static const AkUniqueID FDB = 1267691439U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MC = 1685527061U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID PET = 1182229372U;
+        static const AkUniqueID UI = 1551306167U;
+        static const AkUniqueID VOC = 543694031U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
