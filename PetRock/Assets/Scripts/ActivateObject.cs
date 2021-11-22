7836228305be02b4c603be2964b78ecc6b2fc8e2
Alexis <alexis.sainsard@gmail.com>
2021-11-22 08:15:39 +0100

    Activate Object

---
+++ PetRock/Assets/Scripts/ActivateObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateObject : MonoBehaviour
+{
+    [SerializeField] GameObject objectToActivate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (objectToActivate == null)
+        {
+            Debug.LogError("No object to activate");
+        }
+        objectToActivate.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Plaayer"))
+        {
+            objectToActivate.SetActive(true);
+            Destroy(this.gameObject);
+        }
+    }
+}
