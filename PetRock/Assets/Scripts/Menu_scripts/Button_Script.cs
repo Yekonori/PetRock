cb3572ad825f076b091860a5f0b97e153dad6482
Maxence <mxcolas302@gmail.com>
2021-12-04 20:09:55 +0100

    Update main menu

--- PetRock/Assets/Scripts/Menu_scripts/Button_Script.cs
+++ PetRock/Assets/Scripts/Menu_scripts/Button_Script.cs
@@ -7,22 +7,43 @@ using TMPro;
 
 public class Button_Script : MonoBehaviour
 {
+    [SerializeField]
+    private bool _changeSize = false;
+
     // Update is called once per frame
     void Update()
     {
         if (EventSystem.current.currentSelectedGameObject == gameObject)
         {
             if (GetComponent<Image>() != null)
-                GetComponent<Image>().color = GetComponent<Button>().colors.selectedColor;
+                GetComponent<Image>().sprite = GetComponent<Button>().spriteState.selectedSprite;
             else
                 GetComponent<TextMeshProUGUI>().color = GetComponent<Button>().colors.selectedColor;
+
+            if (_changeSize)
+            {
+                if (GetComponentInChildren<TextMeshProUGUI>() != null)
+                    GetComponentInChildren<TextMeshProUGUI>().fontSizeMax = 50.0f;
+
+                if (GetComponent<RectTransform>() != null)
+                    GetComponent<RectTransform>().sizeDelta = new Vector2(560.0f, 100.0f);
+            }
         }
         else
         {
             if (GetComponent<Image>() != null)
-                GetComponent<Image>().color = GetComponent<Button>().colors.normalColor;
+                GetComponent<Image>().sprite = GetComponent<Button>().spriteState.highlightedSprite;
             else
                 GetComponent<TextMeshProUGUI>().color = GetComponent<Button>().colors.normalColor;
+
+            if (_changeSize)
+            {
+                if (GetComponentInChildren<TextMeshProUGUI>() != null)
+                    GetComponentInChildren<TextMeshProUGUI>().fontSizeMax = 30.0f;
+
+                if (GetComponent<RectTransform>() != null)
+                    GetComponent<RectTransform>().sizeDelta = new Vector2(400.0f, 70.0f);
+            }
         }
     }
 }
