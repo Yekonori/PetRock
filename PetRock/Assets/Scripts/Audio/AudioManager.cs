101c5f5007d121667b0b3483048f38ff349a89f1
Maxence <mxcolas302@gmail.com>
2021-12-09 10:46:02 +0100

    FIx sound play button awake

--- PetRock/Assets/Scripts/Audio/AudioManager.cs
+++ PetRock/Assets/Scripts/Audio/AudioManager.cs
@@ -37,7 +37,7 @@ public class AudioManager : MonoBehaviour
     //Call when a scene is loaded
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        DOTween.To(() => AudioListener.volume, x => AudioListener.volume = x, _globalVolume/100.0f, 2).SetDelay(2);
+        //DOTween.To(() => AudioListener.volume, x => AudioListener.volume = x, _globalVolume/100.0f, 2).SetDelay(2);
     }
 
     void SetVolumes()
